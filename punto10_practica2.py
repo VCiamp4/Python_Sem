a9799071c51f8133ad1f9ee6630fe310d5fb463d
Valentin Ciamparella <66568788+VCiamp4@users.noreply.github.com>
2023-04-09 22:30:32 -0300

    Update punto10_practica2.py

--- punto10_practica2.py
+++ punto10_practica2.py
@@ -12,49 +12,40 @@ notas_2 = [30, 95, 28, 84, 84, 43, 66, 51, 4, 11, 58, 10, 13, 34, 96, 71, 86, 37
 64, 13, 8, 87, 14, 14, 49, 27, 55, 69, 77, 59, 57, 40, 96, 24, 30, 73,
 95, 19, 47, 15, 31, 39, 15, 74, 33, 57, 10]
 
-A = list()
-B = list()
 
-def puntoa(A):
-    A=list(zip(nombres.splitlines(),notas_1,notas_2))
+def puntoa(nombres,notas_1,notas_2):
+    nombres_n = nombres.replace(",","")
+    nombres_n = nombres_n.replace("'","")
+    nombres_n = nombres_n.split()
+    A=list(zip(nombres_n,notas_1,notas_2))
     print(A)
+    return A
 
+def puntob(A):
+    promedios = list(map(lambda x: (x[0], sum(x[1:]) / len(x[1:])), A))
+    print(promedios)
+    return promedios
 
-def puntob(B):
-    prom = [(notas_1 + notas_2)/2 for notas_1,notas_2 in zip(notas_1, notas_2)]
-    b = list(zip(nombres, prom))
-    print(b)
-    return b    
-    
 def puntoc(B):
+    total = 0
     cont = 0
-    prom = 0
     for i in B:
-        cont = cont + 1
-        prom = prom + i[1]
-    prom = prom / cont
-    print(prom)
-        
-def puntoD(B):       
-    maximo = 0
-    nom_max = ""
-    for i in B:
-        if (i[1] > maximo):
-            nom_max = i[0]
-            maximo = i[1]
-    print(f"el alumno con mayor promedio es: {nom_max} con un promedio de {maximo}")
+        total += i[1]
+        cont += 1
+    total = total / cont
+    print (f"promedio total: {total}") 
 
-def puntoE(B):
-    minimo = 9999
-    nom_min = ""
-    for i in B:
-        if (i[1] < minimo):
-            nom_min = i[0]
-            minimo = i[1]
-    print(f"el alumno con el promedio mas bajo:{nom_min} con un promedio de {minimo}")
+def puntod(B):
+    promedio_bajo = min(B,key=lambda x: x[1])
+    print(f"promedio mas bajo: {promedio_bajo[0]}, {promedio_bajo[1]}")
 
-puntoa(A)
-puntob(B)
-puntoc(B)
-puntoD(B)
-puntoE(B)    
+def puntoe(B):
+    promedio_alto = max(B,key=lambda x: x[1])
+    print(f"promedio mas alto: {promedio_alto[0]}, {promedio_alto[1]}")
+
+
+pa = puntoa(nombres,notas_1,notas_2)
+pro =  puntob(pa)
+puntoc(pro)
+puntod(pro)
+puntoe(pro)
